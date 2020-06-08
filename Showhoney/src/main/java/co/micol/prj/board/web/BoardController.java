7a75d5c2ed5bfea8f3ae0c20b78553f74a30eda1
User <User@YD01-09>
2020-06-08 15:58:54 +0900

    layout test

--- Showhoney/src/main/java/co/micol/prj/board/web/BoardController.java
+++ Showhoney/src/main/java/co/micol/prj/board/web/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import co.micol.prj.board.vo.BoardService;
@@ -18,7 +19,7 @@ public class BoardController {
 	
 	@RequestMapping("/main.do")
 	public String main(Model model) {
-		return "main";
+		return "/main/main";
 	}
 
 	@RequestMapping("/boardList.do")
@@ -39,6 +40,11 @@ public class BoardController {
 		mav.addObject("list", list);
 		mav.setViewName("board/boardList");
 		return mav;
+	}
+	
+	@RequestMapping(value="layout.do", method = RequestMethod.GET)
+	public String layout(Model model) {
+		return "layout";
 	}
 	
 }
