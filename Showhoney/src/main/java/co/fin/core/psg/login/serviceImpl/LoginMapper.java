1125cea57ce386dbb1d2426c07f6176596cfcf17
User <User@YD01-18>
2020-06-10 18:09:45 +0900

    i

--- Showhoney/src/main/java/co/fin/core/psg/login/serviceImpl/LoginMapper.java
+++ Showhoney/src/main/java/co/fin/core/psg/login/serviceImpl/LoginMapper.java
@@ -4,5 +4,5 @@ import co.fin.core.psg.login.vo.LoginVo;
 
 public interface LoginMapper {
 
-	public int loginCheck(LoginVo vo);
+	public int loginCheck(LoginVo vo);  // 원래 안 넣어도 자동으로 처리해 주지만 안 될 경우에 이렇게 넣어 주어야 함.
 }
