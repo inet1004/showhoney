3110cac637285898a87c8b0808e824eb1af9666c
User <User@YD01-18>
2020-06-17 16:41:38 +0900

    박석규

--- Showhoney/src/main/java/co/fin/core/psg/login/vo/LoginService.java
+++ Showhoney/src/main/java/co/fin/core/psg/login/vo/LoginService.java
@@ -1,19 +1,19 @@
 package co.fin.core.psg.login.vo;
 
 import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public interface LoginService {
 
 	//고객 로그인 체크
-	public boolean loginCheck(LoginVo vo,HttpSession session);
+	public boolean loginCheck(LoginVo vo, HttpSession session);
 	
 	//기업 로그인 체크
-	public boolean loginCheckCom(LoginComVo cvo,HttpSession session);
+	public boolean loginCheckCom(LoginComVo cvo, HttpSession session);
 	
 	//로그 아웃
 	public void logout(HttpSession session);
-	
-	
+
 }
