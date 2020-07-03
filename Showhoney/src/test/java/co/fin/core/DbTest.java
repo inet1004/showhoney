5712564f7df0b9415ce432d720a4ce7960da96f0
User <User@YD01-20>
2020-07-03 11:19:37 +0900

    2020-07-03 커밋

--- Showhoney/src/test/java/co/fin/core/DbTest.java
+++ Showhoney/src/test/java/co/fin/core/DbTest.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+/*@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/config/context-datasource.xml"}) 
-
+*/
 public class DbTest {
 	  @Inject
 	    private DataSource ds;
